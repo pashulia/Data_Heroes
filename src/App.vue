2729c25d4561ff5424d527c1eddb8c3afdd83441
Pavel Khegay <pashulia.pk@gmail.com>
2024-06-14 11:48:20 +0500

    add CharcterCard and main

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Rick and Morty Characters</h1>
+    <div>
+      <input v-model="filters.name" placeholder="Name" />
+      <select v-model="filters.status">
+        <option value="">Any Status</option>
+        <option value="alive">Alive</option>
+        <option value="dead">Dead</option>
+        <option value="unknown">Unknown</option>
+      </select>
+      <button @click="applyFilters">Apply</button>
+    </div>
+    <div class="characters">
+      <CharacterCard v-for="character in characters" :key="character.id" :character="character" />
+    </div>
+    <div>
+      <button @click="prevPage" :disabled="page === 1">Previous</button>
+      <span>Page {{ page }}</span>
+      <button @click="nextPage">Next</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  onMounted,
+  reactive,
+  ref,
+} from 'vue';
+
+import axios from 'axios';
+
+import CharacterCard from './components/CharacterCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CharacterCard,
+  },
+  setup() {
+    const characters = ref([]);
+    const page = ref(1);
+    const filters = reactive({
+      name: '',
+      status: '',
+    });
+
+    const fetchCharacters = async () => {
+      const { data } = await axios.get('https://rickandmortyapi.com/api/character', {
+        params: {
+          page: page.value,
+          name: filters.name,
+          status: filters.status,
+        },
+      });
+      characters.value = data.results;
+    };
+
+    const applyFilters = () => {
+      page.value = 1;
+      fetchCharacters();
+    };
+
+    const nextPage = () => {
+      page.value++;
+      fetchCharacters();
+    };
+
+    const prevPage = () => {
+      if (page.value > 1) {
+        page.value--;
+        fetchCharacters();
+      }
+    };
+
+    onMounted(() => {
+      fetchCharacters();
+    });
+
+    return {
+      characters,
+      page,
+      filters,
+      applyFilters,
+      nextPage,
+      prevPage,
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+
+.characters {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.character-card {
+  border: 1px solid #ccc;
+  margin: 10px;
+  padding: 10px;
+  width: 300px;
+}
+</style>./components/CharacterCard.vue
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="character-card">
+    <img :src="character.image" alt="Character Image" />
+    <h2>{{ character.name }}</h2>
+    <p>Status: {{ character.status }}</p>
+    <p>Species: {{ character.species }}</p>
+    <p>Gender: {{ character.gender }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterCard',
+  props: {
+    character: Object,
+  },
+};
+</script>
+
+<style>
+.character-card {
+  border: 1px solid #ccc;
+  margin: 10px;
+  padding: 10px;
+  width: 200px;
+}
+</style>
